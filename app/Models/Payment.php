82913e7e00f94383b3e59717df92c544a4e5b327
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-06-23 10:10:31 +0200

    creating Payment model

---
+++ app/Models/Payment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date'
+    ];
+
+
+    /**
+     * Get the user that made the transaction
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
