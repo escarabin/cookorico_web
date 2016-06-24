ea34d0607e25c10903c6070f6e3e013d65fd0cff
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-06-24 11:26:47 +0200

    creating Alert.php model

---
+++ app/Models/Alert.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Alert extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'content'
+    ];
+
+
+    /**
+     * Get the user that created the alert
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
