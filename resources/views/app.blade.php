786ba2e4a53c551f36e91b0c9cfd9d576e4d53fa
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-06-24 11:13:47 +0200

    changed website's title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Angular 2 QuickStart</title>
+    <title>Ouest Emploi CHR</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
