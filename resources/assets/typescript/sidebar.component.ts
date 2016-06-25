f1801e0c7e081ae5875fdca40e60cb0309297d52
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-06-25 13:23:40 +0200

    showing user's data inside sidebar.component

--- resources/assets/typescript/sidebar.component.ts
+++ resources/assets/typescript/sidebar.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+// Services
+import { UserService } from './user.service';
+
 @Component({
+    providers: [UserService],
     selector: 'sidebar',
     templateUrl: '../templates/sidebar.component.html',
 })
 
 export class SidebarComponent {
+    user: any;
 
+    constructor(private userService: UserService) {
+        this.user = JSON.parse(localStorage.getItem('user'));
+    }
 }
