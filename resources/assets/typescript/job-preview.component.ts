41e1537b8fd075970e5fcb07f8b919da96120e3f
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-06-26 20:05:32 +0200

    creating job.component.ts

---
+++ resources/assets/typescript/job-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'job-preview',
+    templateUrl: '../templates/job-preview.component.html',
+})
+
+export class JobPreviewComponent {
+    constructor() {
+
+    }
+}
