be8beb3f7ea84f748e64d22e89b74c5cb97b8634
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-06-29 20:06:23 +0200

    creating experiences.component.ts

---
+++ resources/assets/typescript/experiences.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+import { RouterLink } from '@angular/router-deprecated';
+
+// Services
+import { UserService } from './user.service';
+
+@Component({
+    selector: 'experiences',
+    providers: [UserService],
+    directives: [RouterLink],
+    templateUrl: '../templates/experiences.component.html'
+})
+
+export class ExperiencesComponent {
+    experiences: any;
+
+    constructor(private userService: UserService) {
+        let __this = this;
+
+        this.userService.getExperiences().subscribe((res: Response) => {
+            __this.experiences = res.json();
+        });
+    }
+}
