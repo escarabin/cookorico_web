36390471f933b07f65570c3b9cbd6a85aef54cee
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-07-08 11:31:15 +0200

    allowing cross-domains ajax requests + updating location.service.ts

--- resources/assets/typescript/components/create-business.component.ts
+++ resources/assets/typescript/components/create-business.component.ts
@@ -5,19 +5,22 @@ import { RouterLink } from '@angular/router-deprecated';
 // Services
 import { ReferenceService } from './../services/reference.service';
 import { UserService } from './../services/user.service';
+import { LocationService } from './../services/location.service';
 
 @Component({
     selector: 'create-business',
-    providers: [ReferenceService, UserService],
+    providers: [ReferenceService, UserService, LocationService],
     directives: [RouterLink],
     templateUrl: '../templates/create-business.component.html'
 })
 
 export class CreateBusinessComponent {
     businessTypes: any;
+    adress: string;
 
     constructor(private referenceService: ReferenceService,
-                private userService: UserService) {
+                private userService: UserService,
+                private locationService: LocationService) {
         let __this = this;
 
         this.referenceService.getAllBusinessTypes().subscribe((res: Response) => {
@@ -30,7 +33,8 @@ export class CreateBusinessComponent {
     }
 
     getGoogleMapsAdress() {
-
-        console.log('finding adress...');
+        this.locationService.autocompleteAdress(this.adress).subscribe((res: Response) => {
+            console.log(res.json());
+        })
     }
 }
