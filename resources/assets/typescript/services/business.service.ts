c991737528a5da2509947918f53bd122d35f0c10
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-07-09 20:39:23 +0200

    creating business.service.ts creation function

--- resources/assets/typescript/services/business.service.ts
+++ resources/assets/typescript/services/business.service.ts
@@ -3,8 +3,24 @@ import { HTTP_PROVIDERS, Http } from '@angular/http';
 
 @Injectable()
 export class BusinessService {
+    createBusinessUrl = "/business/create/";
 
     constructor(private http: Http) {
 
+    }
+
+    create(name, adress, postalCode, city, website, typeId, phone, email, description) {
+        let completeUrl = this.createBusinessUrl + '/' +
+            name + '/' +
+            adress + '/' +
+            postalCode + '/' +
+            city + '/' +
+            website + '/' +
+            typeId + '/' +
+            phone + '/' +
+            email + '/' +
+            description;
+
+        return this.http.request(completeUrl);
     }
 }
