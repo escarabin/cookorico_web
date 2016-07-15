044bd8d9429f64f7464277d3b4de30ded85d4137
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-07-15 09:37:36 +0200

    Creating BusinessPhoto.php

---
+++ app/Models/BusinessPhoto.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BusinessPhoto extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title'
+    ];
+
+    /**
+     * Get the business related to this club
+     */
+    public function business()
+    {
+        return $this->hasOne('App\Models\Business');
+    }
+}
