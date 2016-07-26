09c05537d6b5164c62648dfd6313c725f54f6ae8
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-07-26 16:04:52 +0200

    Improving responsiveness on home

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,9 @@
 <head>
     <title>Ouest Emploi CHR</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <!-- Menu bar color on Chrome mobile -->
+    <meta name="theme-color" content="#1a1f38">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
           crossorigin="anonymous">
