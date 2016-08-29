2dd3be2a1c586d18631b24fb5d43a5a14df95572
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-08-29 06:13:14 +0200

    OptionController.php function to save option

--- api/app/Http/Controllers/OptionController.php
+++ api/app/Http/Controllers/OptionController.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Request;
 
+use App\Models\Option;
+
 class OptionController extends Controller
 {
     public function saveOption(Request $request) {
-        Log::info($request::all());
+        $optionData = $request::get('option');
+
+        $option = new Option();
+
+        $option->slug = $optionData['slug'];
+        $option->value = $optionData['value'];
+
+        $option->save();
     }
 }
