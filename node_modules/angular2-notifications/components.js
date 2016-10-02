10fe80f3cde3b4e1488211de6013a9cd247b8751
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-10-02 15:26:11 -0700

    Removing node_modules + js dirs

--- node_modules/angular2-notifications/components.js
+++
@@ -1,3 +0,0 @@
-exports.SimpleNotificationsComponent = require('./lib/simpleNotifications.component').SimpleNotificationsComponent;
-exports.NotificationsService = require('./lib/notifications.service').NotificationsService;
-exports.PushNotificationsService = require('./lib/pushNotifications.service').PushNotificationsService;
