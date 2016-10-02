8e10f1c12b78d61c52cd927d4946a9e455cac5e9
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-10-02 13:04:58 -0700

    Creating Laravel view for each mails

---
+++ api/resources/views/emails/reset-password.php
@@ -0,0 +1,5 @@
+<p>Confirmez votre adresse :&nbsp;</p>
+<p>&nbsp;</p>
+<p>{{ $user->firstName }} {{ $user->lastName }}</p>
+<p>{{ $business->website }}&nbsp;</p>
+<p>&nbsp;</p>{{ $user->user_status_id }}
