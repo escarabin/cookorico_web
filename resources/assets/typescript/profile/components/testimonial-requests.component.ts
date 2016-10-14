26ba55cdaf1123063670f8c17c04f252c52b286a
Emmanuel Scarabin <scarabin.emmanuel@gmail.com>
2016-10-14 10:02:16 -0700

    Handling rejected testimonial requests

--- resources/assets/typescript/profile/components/testimonial-requests.component.ts
+++ resources/assets/typescript/profile/components/testimonial-requests.component.ts
@@ -13,6 +13,7 @@ import { UserService } from '../../services/user.service';
 export class TestimonialRequestsComponent {
     testimonials: any = [];
     accepted_testimonial_requests: any = [];
+    rejected_testimonial_requests: any = [];
 
     constructor(private userService: UserService) {
         let __this = this;
@@ -23,6 +24,9 @@ export class TestimonialRequestsComponent {
             for (let i = 0; i < allTestimonials.length; i++) {
                 if (allTestimonials[i]['is_accepted']) {
                     __this.accepted_testimonial_requests.push(allTestimonials[i]);
+                }
+                else if (allTestimonials[i]['is_rejected']) {
+                    __this.rejected_testimonial_requests.push(allTestimonials[i]);
                 }
                 else {
                     __this.testimonials.push(allTestimonials[i]);
