fe285f31f2ba9c828017fb5c75851d95f0b24e10
Emmanuel Scarabin <escarabin@pc17.home>
2017-04-25 21:43:53 +0200

    Removing generated JS files

--- src/app/models/option.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Option = (function () {
-    function Option(slug, value) {
-        this.slug = slug;
-        this.value = value;
-    }
-    return Option;
-}());
-exports.Option = Option;
